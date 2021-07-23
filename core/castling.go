f6ca9b3cf1c2506dd35d013f3f4d029d87986933
Calvin Figuereo-Supraner <mail@calvin.page>
2021-07-22 22:15:42 -0700

    Add castling

---
+++ core/castling.go
@@ -0,0 +1,31 @@
+package core
+
+import "strings"
+
+type Castling struct {
+	WhiteOO  bool
+	WhiteOOO bool
+	BlackOO  bool
+	BlackOOO bool
+}
+
+func (c Castling) String() string {
+	var sb strings.Builder
+	if c.WhiteOO {
+		sb.WriteString("K")
+	}
+	if c.WhiteOOO {
+		sb.WriteString("Q")
+	}
+	if c.BlackOO {
+		sb.WriteString("k")
+	}
+	if c.BlackOOO {
+		sb.WriteString("q")
+	}
+	s := sb.String()
+	if s == "" {
+		s = "-"
+	}
+	return s
+}
