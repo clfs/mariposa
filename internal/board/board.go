62b8b6c64035a32ad9c7fc0bab3c35ea71676de2
Calvin Figuereo-Supraner <mail@calvin.page>
2021-08-14 10:00:19 -0700

    Add board tests

--- internal/board/board.go
+++ internal/board/board.go
@@ -1,1 +1,74 @@
 package board
+
+import (
+	"github.com/clfs/mariposa/internal/bitboard"
+	"github.com/clfs/mariposa/internal/common"
+)
+
+type Board struct {
+	whites  bitboard.B
+	blacks  bitboard.B
+	pawns   bitboard.B
+	knights bitboard.B
+	bishops bitboard.B
+	rooks   bitboard.B
+	queens  bitboard.B
+	kings   bitboard.B
+}
+
+// Put places p at s and returns b.
+func (b *Board) Put(p common.Piece, s common.Square) *Board {
+	switch p.Color() {
+	case common.White:
+		b.whites.Set(s)
+	case common.Black:
+		b.blacks.Set(s)
+	}
+	switch p.Role() {
+	case common.Pawn:
+		b.pawns.Set(s)
+	case common.Knight:
+		b.knights.Set(s)
+	case common.Bishop:
+		b.bishops.Set(s)
+	case common.Rook:
+		b.rooks.Set(s)
+	case common.Queen:
+		b.queens.Set(s)
+	case common.King:
+		b.kings.Set(s)
+	}
+	return b
+}
+
+func (b *Board) Get(s common.Square) (common.Piece, bool) {
+	var (
+		c common.Color
+		r common.Role
+	)
+	switch {
+	case b.whites.Get(s):
+		c = common.White
+	case b.blacks.Get(s):
+		c = common.Black
+	default:
+		return 0, false
+	}
+	switch {
+	case b.pawns.Get(s):
+		r = common.Pawn
+	case b.knights.Get(s):
+		r = common.Knight
+	case b.bishops.Get(s):
+		r = common.Bishop
+	case b.rooks.Get(s):
+		r = common.Rook
+	case b.queens.Get(s):
+		r = common.Queen
+	case b.kings.Get(s):
+		r = common.King
+	default:
+		return 0, false
+	}
+	return common.NewPiece(c, r), true
+}
--- internal/board/board_test.go
+++ internal/board/board_test.go
@@ -1,1 +1,19 @@
 package board_test
+
+import (
+	"testing"
+	"testing/quick"
+
+	. "github.com/clfs/mariposa/internal/board"
+	"github.com/clfs/mariposa/internal/common"
+)
+
+func TestBoard_PutThenGet(t *testing.T) {
+	f := func(p common.Piece, s common.Square) bool {
+		got, ok := new(Board).Put(p, s).Get(s)
+		return ok && got == p
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
