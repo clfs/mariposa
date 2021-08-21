b2c804aef3f8bd2c025cb22d0c39328059773231
Calvin Figuereo-Supraner <mail@calvin.page>
2021-08-20 20:42:55 -0700

    Add move type

--- internal/common/move.go
+++ internal/common/move.go
@@ -1,7 +1,23 @@
 package common
 
 type Move struct {
-	From, To    Square
-	IsPromotion bool
-	Promotion   Role
+	from, to    Square
+	isPromotion bool
+	promotion   Role
+}
+
+func (m Move) From() Square {
+	return m.from
+}
+
+func (m Move) To() Square {
+	return m.to
+}
+
+func (m Move) IsPromotion() bool {
+	return m.isPromotion
+}
+
+func (m Move) PromotionTo() (Role, bool) {
+	return m.promotion, m.isPromotion
 }
--- internal/movegen/movegen.go
+++ internal/movegen/movegen.go
@@ -8,12 +8,12 @@ import (
 // MaybeLegalMoves returns potentially legal moves for a position. It does not
 // check for legality. For example, it might return a bishop move that causes
 // a king to remain in check.
-func MaybeLegalMoves(p *position.Position) []common.Move {
+func MaybeLegalMoves(p position.Position) []common.Move {
 	return nil
 }
 
 // LegalMoves returns legal moves for a position.
-func LegalMoves(p *position.Position) []common.Move {
+func LegalMoves(p position.Position) []common.Move {
 	var res []common.Move
 	for _, m := range MaybeLegalMoves(p) {
 		if IsLegalMove(p, m) {
@@ -24,6 +24,6 @@ func LegalMoves(p *position.Position) []common.Move {
 }
 
 // IsLegalMove returns true if the move is legal in the position.
-func IsLegalMove(p *position.Position, m common.Move) bool {
+func IsLegalMove(p position.Position, m common.Move) bool {
 	return false
 }
